8cf020573369a1647478e3474a2bbf9b1be73af4
chucks512 <thapelo.machaka14@gmail.com>
2024-05-12 17:45:21 +0200

    set timeout order chapter proj updated

--- advancedJavascript/chapter - (6th copy) -/Set timeout order/scr.js
+++ advancedJavascript/chapter - (6th copy) -/Set timeout order/scr.js
@@ -1,69 +1,17 @@
-/*
-
-let addTwoNumbers = (x, y) => console.log(x + y);
-addTwoNumbers(5, 3);
-
-let sayHi = () => console.log("hi");
-
-const arr = ["squirrel", "alpaca", "buddy"];
-arr.forEach(e => console.log(e));
-
-Four
-Three
-One
-Two
-One
-
-Use the arrow format to create functions that 
-output the values one and two to the console. 
-
-Create a third function that 
-outputs the value three to the console, and then
-invokes the first two functions.
-
-
-Create a fourth function that 
-outputs the word four to the console and also 
-
-use setTimeout() to invoke the first function immediately 
-and then the third function.
-
-*/
-
-//------------------------     console.log("testing...");
-
-/*
-let sayHi = () => console.log("hi");
-//sayHi();
-
+console.log("meeeeee");
 let onePrint = () => console.log("One");
 let twoPrint = () => {console.log("Two")};
 
-function print312(sayHi, onePrint, twoPrint) {
+function print312() {
     console.log("Three");
     onePrint();
     twoPrint();
 }
-function finalPrint(print312, onePrint)
+//--------------------------------------------------------
+function finalPrint()
 {
     console.log("Four");
+    setTimeout(onePrint, 0);
+    print312();
 }
 finalPrint();
-setInterval(print312(), 1000);
-setInterval(onePrint(), 2000);
-*/
-
-
-const one = () => console.log('one');
-const two = () => console.log('two');
-const three = () => {
-    console.log('three');
-    one();
-    two();
-}
-const four = () => {
-    console.log('four');
-    setTimeout(one, 0);
-    three();
-}
-four();
