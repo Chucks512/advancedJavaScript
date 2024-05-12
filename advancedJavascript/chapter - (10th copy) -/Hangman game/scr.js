6ca78c535ca9b1161de2667ac9bda75b40e7236b
chucks512 <thapelo.machaka14@gmail.com>
2024-05-12 19:54:04 +0200

    initialising hangman words array

--- advancedJavascript/chapter - (10th copy) -/Hangman game/scr.js
+++ advancedJavascript/chapter - (10th copy) -/Hangman game/scr.js
@@ -1,5 +1,34 @@
-console.log("tghkjtgjh");
-gameover = 5;
+console.log("testing string...............");
+gameover = 1;
 
-document.getElementById('hangManPic').src= gameover+'.jpg';
+function changePic() {
+    document.getElementById('hangManPic').src= gameover+'.jpg';
+}
+//changePic();
+
+const randomWords = [
+	"Administration",
+	"Appearance",
+	"Atmosphere",
+	"Combination",
+	"Championship",
+	"Conversation",
+	"Distribution",
+	"Establishment",
+	"Independence",
+	"Introduction",
+	"Manufacturer",
+	"Organization",
+	"Performance",
+	"Presentation",
+	"Recommendation",
+	"Responsibility",
+	"Significance",
+	"Supermarket",
+	"Technology",
+	"Transportation"
+];
+
+
+
 
