f90055335d028f007a45cb6e89b0a19608adb7b7
chucks512 <thapelo.machaka14@gmail.com>
2024-05-18 20:17:19 +0200

    hangman java to JS TRANSLATION complete

--- advancedJavascript/chapter - (10th copy) -/Hangman game/scr.js
+++ advancedJavascript/chapter - (10th copy) -/Hangman game/scr.js
@@ -1,46 +1,45 @@
 console.log("testing string...............");
-gameOver = 6;
-
-function printDeadistLevel(gameover) {
-    document.getElementById('hangManPic').src = gameover+'.jpg';
-}
-//printDeadistLevel(gameover);
-function output(theString){
-outTake = document.getElementById('outputHere');
-outTake.innerHTML = theString;
-}
-function input(){
-    return document.getElementById('userInput').value ;
-
-}
+gameOver = 0;
 
 const randomList = [
-	"Administration",
-	"Appearance",
-	"Atmosphere",
-	"Combination",
-	"Championship",
-	"Conversation",
-	"Distribution",
-	"Establishment",
-	"Independence",
-	"Introduction",
-	"Manufacturer",
-	"Organization",
-	"Performance",
-	"Presentation",
-	"Recommendation",
-	"Responsibility",
-	"Significance",
-	"Supermarket",
-	"Technology",
-	"Transportation"
+    "Administration",
+    "Appearance",
+    "Atmosphere",
+    "Combination",
+    "Championship",
+    "Conversation",
+    "Distribution",
+    "Establishment",
+    "Independence",
+    "Introduction",
+    "Manufacturer",
+    "Organization",
+    "Performance",
+    "Presentation",
+    "Recommendation",
+    "Responsibility",
+    "Significance",
+    "Supermarket",
+    "Technology",
+    "Transportation"
 ];
 
+function printDeadistLevel(gameover) {
+    document.getElementById('hangManPic').src = gameover + '.jpg';
+}
+//printDeadistLevel(gameover);
+function output(theString) {
+    outTake = document.getElementById('outputHere');
+    outTake.innerHTML = theString;
+}
+function input() {
+    return document.getElementById('userInput').value;
+
+}
 
 
+//-------------------------------------------------------------
 
-/*
 
 charByIndexMap = new Map();
 wordCharsSet = new Set();
@@ -48,7 +47,7 @@ userRandomsSet = new Set();
 let randomWord;
 let userRandomWord;
 
-function StringHandler(){
+function StringHandler() {
     randomWord = getRandomWord(randomList);
     randomWord = randomWord.toUpperCase();
     wordToHashSet();
@@ -58,11 +57,11 @@ function StringHandler(){
     theLoop();
 }
 
-function theLoop(){
-    while(true){
+function theLoop() {
+    while (true) {
         alert(randomWord + "YOU WIN!!!")
         fail;
-    }
+    }//----------------------------------logic broer
     printDeadistLevel(gameOver);
     output(userRandomWord);
     alert("enter more inputs");
@@ -70,7 +69,7 @@ function theLoop(){
     charUserEntry = userEntry
     checkUserCorrecttnesss(charUserEntry);
 }
- `  `
+
 checkUserCorrecttnesss(userInput){
     strUserInput = input();
     userInput = strUserInput.toUpperCase();
@@ -81,14 +80,87 @@ checkUserCorrecttnesss(userInput){
     arrUserRandomWord = userRandomWord.split("");
     let coordinates = [];
     coordinates = charByIndexMap.get(input());
-    //
-    if (userRandomsSet.includes(input())){
+    
+    if (userRandomsSet.includes(input())) {
         userRandomsSet.delete(input());
         counter = coordinates.length;
 
-        for ()
+        for (let i of coordinates) {
+            counter = copyRandomWord.indexOf(strUserInput);
+            copyRandomWord = copyRandomWord.replace(strUserInput, "_");
+            builderUserRandomWord[counter] = userInput;
+
+            arrUserRandomWord[counter] = userInput;
+        }
+        userRandomWord = "";
+        for (let myChar of arrUserRandomWord) {
+            userRandomWord += myChar;
+        }
+    } else {
+        gameOver++;
+        if (gameOver > 6) {
+            console.log("you lose!! " + randomWord);
+            fail;
+        }
+    }
 }
 
+function getRandomWord(myList) {
+    randomWord = myList[Math.floor(Math.random() * myList.length)];
+    return randomWord;
+}
 
+function wordToHashSet() {
+    for (let i = 0; i < randomWord.length(); i++) {
+        wordCharsSet.add(randomWord.charAt(i));
+    }
+}
 
-*/
+function threeRandoms() {
+    let arrRandomword = randomWord.split("");
+    counter = 0;
+    while (counter < 3) {
+        randomLetter = arrRandomword[Math.floor(Math.random() * arrRandomword.length)]
+        if (true == (userRandomsSet.add(randomLetter))) {
+            counter++;
+        }
+    }
+}
+
+function storeCharIndexes() {
+    let counter;
+    let currentLetter;
+
+    let tempRandomWord = [];
+    for (let myChar of randomWord.split("")) {
+        tempRandomWord = tempRandomWord.push(myChar);
+    }
+
+    let wordCharsSet = [];
+    for (let myChar of wordCharsSet) {
+        wordCharsSetClone = wordCharsSetClone.push(myChar);
+    }
+
+    for (let i = 0; i < wordCharsSetClone.length(); i++) {
+        let tempArrListIndex = [];
+        //line 128, we will index hashset later and get (i)
+        //currentLetter = wordCharsSetClone.ge
+        while (true) {
+            counter = tempRandomWord.indexOf(currentLetter);
+            if (counter == -1) {
+                charByIndexMap.set(currentLetter, tempArrListIndex);
+                break;
+            }
+            tempRandomWord[counter] = "_";
+            tempArrListIndex = tempArrListIndex.push(counter);
+        }
+    }
+}
+
+function dampenWord() {
+    userRandomWord = randomWord;
+    for (let i of userRandomsSet) {
+        temp = i;
+        userRandomWord = userRandomWord.replaceAll(temp, "_");
+    }
+}
