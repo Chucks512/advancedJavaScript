d9438d9db2a818312e64ced88c3b193e9c81ce8a
chucks512 <thapelo.machaka14@gmail.com>
2024-05-22 11:36:45 +0200

    getRandomWord() function complete

--- advancedJavascript/chapter - (10th copy) -/Hangman game/scr.js
+++ advancedJavascript/chapter - (10th copy) -/Hangman game/scr.js
@@ -37,17 +37,29 @@ function input() {
 
 }
 
-
-//-------------------------------------------------------------
-/*
-
 charByIndexMap = new Map();
 wordCharsSet = new Set();
 userRandomsSet = new Set();
 let randomWord;
 let userRandomWord;
+//----------------------------------------------------------------THE ABOVE IS ALL GOOD
 
-function StringHandler() {
+randomWord = "Hello";
+
+function wordToHashSet() { //---------------------------------------TO BE APPROVED
+    for (let i = 0; i < randomWord.length(); i++) {
+        wordCharsSet.add(randomWord.charAt(i));
+    }
+}
+
+wordToHashSet();
+console.log(wordCharsSet);
+
+
+
+//------------------------------------------------------------- BELOW FUNCTIONS TO BE APPROVED
+/*
+function StringHandler() { //---------------------------------------TO BE APPROVED
     randomWord = getRandomWord(randomList);
     randomWord = randomWord.toUpperCase();
     wordToHashSet();
@@ -57,7 +69,7 @@ function StringHandler() {
     theLoop();
 }
 
-function theLoop() {
+function theLoop() { //---------------------------------------TO BE APPROVED
     while (true) {
         if (userRandomWord == randomWord) {
             alert(randomWord + "YOU WIN!!!")
@@ -75,7 +87,7 @@ function theLoop() {
     checkUserCorrecttnesss(charUserEntry);
 }
 
-function checkUserCorrecttnesss(userInput) {
+function checkUserCorrecttnesss(userInput) { //---------------------------------------TO BE APPROVED
     strUserInput = input();
     userInput = strUserInput.toUpperCase();
     copyRandomWord = randomWord;
@@ -110,18 +122,18 @@ function checkUserCorrecttnesss(userInput) {
     }
 }
 
-function getRandomWord(myList) {
+function getRandomWord(myList) { //--------------------------------------- YES
     randomWord = myList[Math.floor(Math.random() * myList.length)];
     return randomWord;
 }
 
-function wordToHashSet() {
+function wordToHashSet() { //---------------------------------------TO BE APPROVED
     for (let i = 0; i < randomWord.length(); i++) {
         wordCharsSet.add(randomWord.charAt(i));
     }
 }
 
-function threeRandoms() {
+function threeRandoms() { //---------------------------------------TO BE APPROVED
     let arrRandomword = randomWord.split("");
     counter = 0;
     while (counter < 3) {
@@ -132,7 +144,7 @@ function threeRandoms() {
     }
 }
 
-function storeCharIndexes() {
+function storeCharIndexes() { //---------------------------------------TO BE APPROVED
     let counter;
     let currentLetter;
 
@@ -162,7 +174,7 @@ function storeCharIndexes() {
     }
 }
 
-function dampenWord() {
+function dampenWord() { //---------------------------------------TO BE APPROVED
     userRandomWord = randomWord;
     for (let i of userRandomsSet) {
         temp = i;
