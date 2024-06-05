9e1842d99e404ec3fe6e427076175e8778587d08
chucks512 <thapelo.machaka14@gmail.com>
2024-06-05 18:12:45 +0200

    star rating system complete

--- advancedJavascript/chapter - (11th copy) -/Star rating system/scr.js
+++ advancedJavascript/chapter - (11th copy) -/Star rating system/scr.js
@@ -1,1 +1,23 @@
-document.getElementById('hangManPic').src='1.jpg';
+let stars = document.querySelectorAll("li");
+console.log(stars);
+
+stars.forEach(element => {
+    element.addEventListener("click", function () {
+        for (let index = 0; index <5; index++) {
+            stars[index].style.color = "#ddd";
+        }
+        event.target.style.color = "blue";
+        let currentStar = event.target;
+       
+        for (let index = 0; index <4; index++) {
+            currentStar.previousElementSibling.style.color = "blue";
+            currentStar = currentStar.previousElementSibling;
+        }
+    });
+});
+
+
+
+
+
+
