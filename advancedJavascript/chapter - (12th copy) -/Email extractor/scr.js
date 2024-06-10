83c9fe843f3d45a5dca592bab754d5bb77f03810
chucks512 <thapelo.machaka14@gmail.com>
2024-06-10 22:54:45 +0200

    email extractor complete

--- advancedJavascript/chapter - (12th copy) -/Email extractor/scr.js
+++ advancedJavascript/chapter - (12th copy) -/Email extractor/scr.js
@@ -4,9 +4,27 @@ getEmailBtn.addEventListener("click", testFunc);
 let textarea = document.getElementById("textarea");
 let textarea2 = document.getElementById("textarea2");
 
+emailSet = new Set();
+
 function testFunc() {
-    console.dir(document.querySelectorAll("textarea"));
-    x = document.querySelectorAll("textarea");
-    x[0].innerText = "testing...";
-    console.log(x[0].innerText);//try get methods
+    console.clear();
+    textAreaArray = document.querySelectorAll("textarea");
+    theEmail = textAreaArray[0].value;
+    textAreaArray[0].value = "";
+
+    let emailPattern = /([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/g;
+    testAnswer = (theEmail.match(emailPattern));
+
+    if (testAnswer != null) {
+        console.log(theEmail + " - not null");
+
+        if (textAreaArray[1].value.includes(theEmail)) {
+            console.log(theEmail + " - email already exists");
+        } else {
+            textAreaArray[1].value = theEmail + "\n" + textAreaArray[1].value;
+        }
+
+    } else {
+        console.log(theEmail + " - is null");
+    }
 }
