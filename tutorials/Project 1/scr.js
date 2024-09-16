1a6da4c1c3d2aca413d64efc4321b57233de6e58
chucks512 <thapelo.machaka14@gmail.com>
2024-09-16 21:41:58 +0200

    get select input working

--- tutorials/Project 1/scr.js
+++ tutorials/Project 1/scr.js
@@ -1,1 +1,10 @@
-//ghk
+var min = 12,
+    max = 100,
+    select = document.getElementById('keySelector');
+
+for (var i = min; i<=max; i++){
+    var opt = document.createElement('option');
+    opt.value = i;
+    opt.innerHTML = i;
+    select.appendChild(opt);
+}
