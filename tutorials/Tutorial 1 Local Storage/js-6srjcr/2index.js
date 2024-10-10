b11c3ab7e2311bdf92ef96cae2151182adf132cd
chucks512 <thapelo.machaka14@gmail.com>
2024-10-10 18:46:01 +0200

    finishing up tutorial

--- tutorials/Tutorial 1 Local Storage/js-6srjcr/2index.js
+++ tutorials/Tutorial 1 Local Storage/js-6srjcr/2index.js
@@ -1,133 +1,125 @@
 // import './index.css';
 
-const localTodosContainer = document.getElementById(
-  'local-storage-todos-container'
-);
-const localInputEle = document.getElementById('local-storage-todo-input-ele');
-const localAddTaskBtn = document.getElementById('local-storage-add-task-btn');
+const localTodosContainer = document.getElementById('local-storage-todos-container');//local section div
+const localInputEle = document.getElementById('local-storage-todo-input-ele');//text inputbox
+const localAddTaskBtn = document.getElementById('local-storage-add-task-btn');//add button
 
-function localcreateTodoLiElements(localtodoArray) {
-  return localtodoArray.map((i, index) => {
-    const liElement = document.createElement('li');
-    const localcheckboxEle = document.createElement('input');
-    const labelEle = document.createElement('label');
+function localcreateTodoLiElements(localtodoArray) {//takes in array, makes item elements
+  return localtodoArray.map((i, index) => {//map = send one by one and run code
+    const liElement = document.createElement('li');//item element creation
+    const localcheckboxEle = document.createElement('input');// input(checkbox later) creation
+    const labelEle = document.createElement('label');//text element
 
-    localcheckboxEle.setAttribute('type', 'checkbox');
-    localcheckboxEle.setAttribute('id', `local-chbx-${index}`);
-    labelEle.setAttribute('for', `local-chbx-${index}`);
+    localcheckboxEle.setAttribute('type', 'checkbox');//specify checkbox
+    localcheckboxEle.setAttribute('id', `local-chbx-${index}`);//set id to text + id concatenation
+    labelEle.setAttribute('for', `local-chbx-${index}`);//set id to text + id concatenation
 
-    if (i.checked) {
-      localcheckboxEle.checked = true;
-      labelEle.classList.add('todo-task-done');
+    if (i.checked) {//verifies if checked
+      localcheckboxEle.checked = true;//applies the same to frontend
+      labelEle.classList.add('todo-task-done');//add to done-list
     }
 
-    localcheckboxEle.addEventListener('click', (e) => {
-      const todoArr = JSON.parse(localStorage.getItem('codesweetlyStore'));
+    localcheckboxEle.addEventListener('click', (e) => {//add event listener
+      const todoArr = JSON.parse(localStorage.getItem('codesweetlyStore'));//get local storage item json to String
       todoArr[e.target.getAttribute('id').split('-')[2]].checked =
-        !todoArr[e.target.getAttribute('id').split('-')[2]].checked;
-      localStorage.setItem('codesweetlyStore', JSON.stringify(todoArr));
-      labelEle.classList.toggle('todo-task-done');
+        !todoArr[e.target.getAttribute('id').split('-')[2]].checked;//sets the checked property
+      localStorage.setItem('codesweetlyStore', JSON.stringify(todoArr));//set backend to todoArr
+      labelEle.classList.toggle('todo-task-done');//check the done checkbox
     });
 
-    labelEle.textContent = i.text;
-    liElement.append(localcheckboxEle, labelEle);
-    return liElement;
+    labelEle.textContent = i.text;//set text to component
+    liElement.append(localcheckboxEle, labelEle);//send to front end
+    return liElement;//return to whoever called
   });
 }
 
 window.addEventListener(
-  'load',
+  'load',//add event listener on load
   (() => {
     const locallocalTodoArray =
-      JSON.parse(localStorage.getItem('codesweetlyStore')) || [];
-    const todoLiElements = localcreateTodoLiElements(locallocalTodoArray);
-    localTodosContainer.replaceChildren(...todoLiElements);
+      JSON.parse(localStorage.getItem('codesweetlyStore')) || [];//set all local storage items to array
+    const todoLiElements = localcreateTodoLiElements(locallocalTodoArray);//create item elements for each
+    localTodosContainer.replaceChildren(...todoLiElements);//replace front end items with backend items
     // Log the the local storage's zeroth index item's name to the console:
     console.log(localStorage.key(0));
   })()
 );
 
-localAddTaskBtn.addEventListener('click', () => {
+localAddTaskBtn.addEventListener('click', () => {//listener to button
   const currentlocalTodoArray =
-    JSON.parse(localStorage.getItem('codesweetlyStore')) || [];
+    JSON.parse(localStorage.getItem('codesweetlyStore')) || [];//set all local storage items to array
   const newlocalTodoArray = [
-    ...currentlocalTodoArray,
-    { checked: false, text: localInputEle.value },
+    ...currentlocalTodoArray,//append from already existing items
+    { checked: false, text: localInputEle.value },// add new item with unchecked box
   ];
-  const todoLiElements = localcreateTodoLiElements(newlocalTodoArray);
-  localStorage.setItem('codesweetlyStore', JSON.stringify(newlocalTodoArray));
-  localTodosContainer.replaceChildren(...todoLiElements);
-  localInputEle.value = '';
+  const todoLiElements = localcreateTodoLiElements(newlocalTodoArray);//create item
+  localStorage.setItem('codesweetlyStore', JSON.stringify(newlocalTodoArray));//set it to backend local storage
+  localTodosContainer.replaceChildren(...todoLiElements);//replace frontend component children
+  localInputEle.value = '';//reset input value to nothing
 });
 
 
 
 
-
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
 // import './index.css';
 
-const sessionTodosContainer = document.getElementById(
-  'session-storage-todos-container'
-);
-const sessionInputEle = document.getElementById(
-  'session-storage-todo-input-ele'
-);
-const sessionAddTaskBtn = document.getElementById(
-  'session-storage-add-task-btn'
-);
+const sessionTodosContainer = document.getElementById('session-storage-todos-container');//local section div
+const sessionInputEle = document.getElementById('session-storage-todo-input-ele');//text inputbox
+const sessionAddTaskBtn = document.getElementById('session-storage-add-task-btn');//add button
 
-function sessioncreateTodoLiElements(sessiontodoArray) {
-  return sessiontodoArray.map((i, index) => {
-    const liElement = document.createElement('li');
-    const session = document.createElement('input');
-    const labelEle = document.createElement('label');
+function sessioncreateTodoLiElements(sessiontodoArray) {//takes in array, makes item elements
+  return sessiontodoArray.map((i, index) => {//map = send one by one and run code
+    const liElement = document.createElement('li');//item element creation
+    const session = document.createElement('input');// input(checkbox later) creation
+    const labelEle = document.createElement('label');//text element
 
-    session.setAttribute('type', 'checkbox');
-    session.setAttribute('id', `session-chbx-${index}`);
-    labelEle.setAttribute('for', `session-chbx-${index}`);
+    session.setAttribute('type', 'checkbox');//specify checkbox
+    session.setAttribute('id', `session-chbx-${index}`);//set id to text + id concatenation
+    labelEle.setAttribute('for', `session-chbx-${index}`);//set id to text + id concatenation
 
-    if (i.checked) {
-      session.checked = true;
-      labelEle.classList.add('todo-task-done');
+    if (i.checked) {//verifies if checked
+      session.checked = true;//applies the same to frontend
+      labelEle.classList.add('todo-task-done');//add to done-list
     }
 
-    session.addEventListener('click', (e) => {
-      const todoArr = JSON.parse(sessionStorage.getItem('codesweetlyStore'));
+    session.addEventListener('click', (e) => {//add event listener
+      const todoArr = JSON.parse(sessionStorage.getItem('codesweetlyStore'));//get local storage item json to String
       todoArr[e.target.getAttribute('id').split('-')[2]].checked =
-        !todoArr[e.target.getAttribute('id').split('-')[2]].checked;
-      sessionStorage.setItem('codesweetlyStore', JSON.stringify(todoArr));
-      labelEle.classList.toggle('todo-task-done');
+        !todoArr[e.target.getAttribute('id').split('-')[2]].checked;//sets the checked property
+      sessionStorage.setItem('codesweetlyStore', JSON.stringify(todoArr));//set backend to todoArr
+      labelEle.classList.toggle('todo-task-done');//check the done checkbox
     });
 
-    labelEle.textContent = i.text;
-    liElement.append(session, labelEle);
-    return liElement;
+    labelEle.textContent = i.text;//set text to component
+    liElement.append(session, labelEle);//send to front end
+    return liElement;//return to whoever called
   });
 }
 
 window.addEventListener(
-  'load',
+  'load',//add event listener on load
   (() => {
     const sessionsessionTodoArray =
-      JSON.parse(sessionStorage.getItem('codesweetlyStore')) || [];
-    const todoLiElements = sessioncreateTodoLiElements(sessionsessionTodoArray);
-    sessionTodosContainer.replaceChildren(...todoLiElements);
+      JSON.parse(sessionStorage.getItem('codesweetlyStore')) || [];//set all local storage items to array
+    const todoLiElements = sessioncreateTodoLiElements(sessionsessionTodoArray);//create item elements for each
+    sessionTodosContainer.replaceChildren(...todoLiElements);//replace front end items with backend items
     // Log the total items in the browser's session storage to the console:
     console.log(sessionStorage.length);
   })()
 );
 
-sessionAddTaskBtn.addEventListener('click', () => {
+sessionAddTaskBtn.addEventListener('click', () => {//listener to button
   const currentsessionTodoArray =
-    JSON.parse(sessionStorage.getItem('codesweetlyStore')) || [];
+    JSON.parse(sessionStorage.getItem('codesweetlyStore')) || [];//set all local storage items to array
   const newsessionTodoArray = [
-    ...currentsessionTodoArray,
-    { checked: false, text: sessionInputEle.value },
+    ...currentsessionTodoArray,//append from already existing items
+    { checked: false, text: sessionInputEle.value },// add new item with unchecked box
   ];
-  const todoLiElements = sessioncreateTodoLiElements(newsessionTodoArray);
-  sessionStorage.setItem('codesweetlyStore', JSON.stringify(newsessionTodoArray));
-  sessionTodosContainer.replaceChildren(...todoLiElements);
-  sessionInputEle.value = '';
+  const todoLiElements = sessioncreateTodoLiElements(newsessionTodoArray);//create item
+  sessionStorage.setItem('codesweetlyStore', JSON.stringify(newsessionTodoArray));//set it to backend local storage
+  sessionTodosContainer.replaceChildren(...todoLiElements);//replace frontend component children
+  sessionInputEle.value = '';//reset input value to nothing
 });
